e6414f91f22378d257cd1038cc2e9a489f585d8d
GOODING Andrew <agooding@altranuk.local>
2019-09-13 11:19:09 +0100

    Created unit tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -3,12 +3,115 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
+
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_passes_under_5(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 2, 30)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(33, items[0].quality)
+
+    def test_passes_under_10(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 8, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(32, items[0].quality)
+
+    def test_passes(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 12, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(31, items[0].quality)
+
+    def test_passes_expired(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 24)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(0, items[0].quality)
+
+    def test_passes_max_10(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 9, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
+
+    def test_passes_max_5(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
+
+    def test_passes_max(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 13, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
+
+    def test_dexterity_vest(self):
+        items = [Item("+5 Dexterity Vest", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(19, items[0].quality)
+
+    def test_dexterity_vest_expired(self):
+        items = [Item("+5 Dexterity Vest", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(18, items[0].quality)
+
+    def test_elixir(self):
+        items = [Item("Elixir of the Mongoose", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(19, items[0].quality)
+
+    def test_elixir_expired(self):
+        items = [Item("Elixir of the Mongoose", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(18, items[0].quality)
+
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 10, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(80, items[0].quality)
+
+    def test_sulfuras_expired(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(80, items[0].quality)
+
+    def test_brie(self):
+        items = [Item("Aged Brie", 10, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(1, items[0].quality)
+
+    def test_brie_expired(self):
+        items = [Item("Aged Brie", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(2, items[0].quality)
+
+    def test_brie_max(self):
+        items = [Item("Aged Brie", 12, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
+
+    def test_conjured(self):
+        items = [Item("Conjured Mana Cake", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(18, items[0].quality)
+
+    def test_conjured_expired(self):
+        items = [Item("Conjured Mana Cake", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(16, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -4,10 +4,9 @@ from __future__ import print_function
 from gilded_rose import *
 
 if __name__ == "__main__":
-    print ("OMGHAI!")
     items = [
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
+             Item(name="Aged Brie", sell_in=6, quality=0),
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
@@ -17,14 +16,15 @@ if __name__ == "__main__":
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
-    days = 2
+    days = 12
     import sys
-    if len(sys.argv) > 1:
-        days = int(sys.argv[1]) + 1
-    for day in range(days):
-        print("-------- day %s --------" % day)
-        print("name, sellIn, quality")
-        for item in items:
-            print(item)
-        print("")
-        GildedRose(items).update_quality()
+    with open("Test_Result.txt", 'w+') as file:
+        if len(sys.argv) > 1:
+            days = int(sys.argv[1]) + 1
+        for day in range(days):
+            print("-------- day %s --------" % day, file=file)
+            print("name, sellIn, quality", file=file)
+            for item in items:
+                print(item, file=file)
+            print("", file=file)
+            GildedRose(items).update_quality()
